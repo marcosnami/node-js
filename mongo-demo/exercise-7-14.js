7fdf45fd12835077190905754db763986b010f85
Marcos Nami <marcosnami@hotmail.com>
2018-11-20 08:46:51 -0200

    Third Commit

---
+++ mongo-demo/exercise-7-14.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises', { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(err => console.log("Could not connect to MongoDB..."));
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+    return await Course
+    .find({ isPublished: true, tags: 'backend' })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1, _id: 0 });
+}
+
+async function run() {
+    try {
+        const courses = await getCourses();
+        console.log(courses);
+    } catch(err) {
+        console.log(new Error("Error getting courses...", err.message));
+    }
+}
+
+run();
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -6,11 +6,57 @@ mongoose.connect('mongodb://localhost/payground', { useNewUrlParser: true })
 
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 5,
+        maxlength: 255
+        // match: /pattern/
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'],
+        lowercase: true,
+        //uppercase: true,
+        trim: true
+    },
     author: String,
-    tags: [ String ],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean
+    tags: {
+        type: Array,
+        // Sync Validation
+        // validate: {
+        //     validator: function(v) {
+        //         return v && v.length > 0;
+        //     },
+        //     message: 'A course should have at least one tag.'
+        // }
+        // Async Validation
+        validate: {
+            isAsync: true,
+            validator: function(v, callback) {
+                setTimeout(() => {
+                    const result = v && v.length > 0;
+                    callback(result);
+                }, 4000)
+            },
+            message: 'A course should have at least one tag.'
+        }
+    },
+    date: { 
+        type: Date,
+        default: Date.now
+    },
+    isPublished: Boolean,
+    price: { 
+        type: Number,
+        min: 10,
+        max: 200,
+        //required: function() { return this.isPublished; },
+        required: function() { return this.isPublished; },
+        get: v => Math.round(v),
+        set: v => Math.round(v)
+    }
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -19,15 +65,22 @@ async function createCourse() {
     try {
         const course = new Course({
             name: "Angular Course",
-            author: "Mosh",
-            tags: ["angular", "frontend"],
-            isPublished: true
+            author: "Jason",
+            category: "Web",
+            tags: [ 'angular', 'frontend' ],
+            //tags: null,
+            isPublished: true,
+            price: 12.8
         });
 
         const result = await course.save();
         console.log(result);
     } catch(err) {
-        console.log(new Error("Error: ", err.message));
+        //console.log("Error: ", err.message);
+        //console.log(err.errors.category);
+        for(field in err.errors) {
+            console.log(err.errors[field].message);
+        }  
     }
 }
 
@@ -51,6 +104,10 @@ async function getCourses() {
     // ($) Ends with
     // (.*) Contains it
     try {
+        const pageNumber = 1;
+        const pageSize = 10;
+        // /api/courses?pageNumber=2&pageSize=10
+
         const courses = await Course
             //.find({ author: "Mosh", isPublished: true })
             //.find({ price: { $gte: 10, $lte: 20 } })
@@ -59,9 +116,11 @@ async function getCourses() {
             //.or([ { author: "Mosh" }, { isPublished: true } ])
             //.and([ { author: "Mosh" }, { isPublished: true }, { name: "Angular Course"} ])
             //.limit(10)
+            .skip((pageNumber - 1) * pageSize)
+            .limit(pageSize)
             //.sort({ name: 1 })
             //.select({ name: 1, tags: 1, _id: 0 })
-            .countDocuments()
+            //.countDocuments()
             //.estimatedDocumentCount();
         console.log(courses);
     } catch(err) {
@@ -69,7 +128,60 @@ async function getCourses() {
     }
 }
 
+async function updateCourse(id) {
+    // Approach: Query first
+    // findById();
+    // Modify its properties
+    // save()
+    // const course = await Course
+    //     .findById(id);
+    // if(!course) {
+    //     console.log("There is not a course with id: ", id);
+    //     return;
+    // }
 
-//createCourse();
-getCourses();
+    // // course.isPublished = true;
+    // // course.author = 'Another Author';
 
+    // course.set({
+    //     isPublished: true,
+    //     author: 'Another Great Author'
+    // });
+
+    // const result = await course.save();
+    // console.log(result);
+
+    // Approach: Update directly into DB
+    // const result = await Course.updateOne({ _id: id }, {
+    //     $set: {
+    //         author: "Mosh",
+    //         isPublished: false
+    //     }
+    // });
+    // console.log(result);
+
+    // Update a return a course
+    const course = await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: "Jack",
+            isPublished: true
+        }
+    }, { new: true });
+
+    console.log(course);
+
+}
+
+async function removeCourse(id) {
+    // const result = await Course.deleteOne({ _id: id });
+    // console.log(result);
+    const course = await Course.findByIdAndDelete(id);
+    console.log(course);
+
+}
+
+
+createCourse();
+//getCourses();
+//updateCourse("5bf0c8f68829e05e676e2617");
+//removeCourse("5bf0ca0e4850b56092128941");
